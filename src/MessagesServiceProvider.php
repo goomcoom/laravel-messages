a4ab2d87695c15c4dc43163a4247acd7ba5d807a
Simon Msara <simonmsaraone@gmail.com>
2020-06-16 21:26:05 +0100

    Added the service provider

---
+++ src/MessagesServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GoomCoom\Messages;
+
+use Illuminate\Support\ServiceProvider;
+
+class MessagesServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
