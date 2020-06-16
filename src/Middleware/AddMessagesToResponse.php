8311437e129273178932df4f5997b7a42bf05ead
Simon Msara <simonmsaraone@gmail.com>
2020-06-16 22:00:54 +0100

    Created the middleware

---
+++ src/Middleware/AddMessagesToResponse.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace GoomCoom\Messages\Middleware;
+
+use Closure;
+use GoomCoom\Messages\Messages;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+
+class AddMessagesToResponse
+{
+    /**
+     * @var Application|ResponseFactory|mixed
+     */
+    protected ResponseFactory $factory;
+
+    /**
+     * AddMessagesToResponse constructor.
+     */
+    public function __construct()
+    {
+        $this->factory = app(ResponseFactory::class);
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return JsonResponse
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (!$response instanceof JsonResponse) {
+
+            $response = $this->factory->json(
+                $response->content(),
+                $response->status(),
+                $response->headers->all()
+            );
+        }
+
+        return $this->addMessages($response);
+    }
+
+    /**
+     * Check if there are any messages and add them to the response.
+     *
+     * @param JsonResponse $response
+     * @return JsonResponse
+     */
+    protected function addMessages(JsonResponse $response) :JsonResponse
+    {
+        $data = $response->getData(true);
+
+        if (gettype($data) === 'string' && $data) {
+            $data = json_decode($data, true);
+        }
+
+        if (!$data) $data = [];
+
+        if (Messages::hasMessages()) $data['messages'] = Messages::allMessages();
+
+        if (isset($data['message'])) {
+            $data['messages']['error'][] = $data['message'];
+            unset($data['message']);
+        }
+
+        return $response->setData($data);
+    }
+}
