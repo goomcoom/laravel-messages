ede6cf9a6011aa5520c3e60276114518c8e554fa
Simon Msara <simonmsaraone@gmail.com>
2020-06-16 21:33:28 +0100

    Created the messages facade

---
+++ src/Facades/Messages.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * The messages facade.
+ *
+ * @method static getBags()
+ * @method static hasMessages()
+ * @method static allMessages()
+ * @method static getBag(string $name)
+ * @method static add(string $bag, int|string|array ...$messages)
+ */
+class Messages extends Facade
+{
+    /**
+     * Return the name of the class that has been registered.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() :string
+    {
+        return 'messages';
+    }
+}
